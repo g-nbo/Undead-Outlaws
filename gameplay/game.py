82c1982f82068c1d0b6973b3ecd9a68545de4adb
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-10-22 16:10:58 -0400

    Add enemy and player health

--- gameplay/game.py
+++ gameplay/game.py
@@ -4,40 +4,70 @@ print("hello world!")
 pygame.init()
 
 # Create screen
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.6)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Undead Outlaws")
 
 # Create initial character
-
 x = 200
 y = 200
-scale = 0.5
+scale = 0.3
 
-img = pygame.image.load("../images/test-sprite-game.png")
-img = pygame.transform.scale(img, (img.get_width() * scale, img.get_height() * scale))
+characterSprite = pygame.image.load("../images/test-sprite-game.png")
+characterSprite = pygame.transform.scale(characterSprite, (characterSprite.get_width() * scale, characterSprite.get_height() * scale))
 
-rect = img.get_rect()
-rect.center = (x, y)
+charRect = characterSprite.get_rect()
+charRect.center = (0 + 70, SCREEN_HEIGHT - 70)
 
+playerHealth = 5
 
+# Create enemy
+enemySprite = pygame.image.load("../images/dino-enemy-sprite.png")
+enemySprite = pygame.transform.scale(enemySprite, (enemySprite.get_width() * scale, enemySprite.get_height() * scale))
+
+enemyRect = enemySprite.get_rect()
+enemyRect.center = (SCREEN_WIDTH - 70, SCREEN_HEIGHT - 70)
+eHasHit = False
+
+# Time
+clock = pygame.time.Clock()
 
 # Create basic game loop
-
 run = True
 while run:
+    screen.fill(("BLACK"))
 
-    screen.blit(img, rect)
+    screen.blit(characterSprite, charRect)
+    screen.blit(enemySprite, enemyRect)
+    keys = pygame.key.get_pressed()
 
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             run = False
 
+        if keys[pygame.K_a] and charRect.left > -30:
+             charRect = charRect.move(-15, 0)
+        if keys[pygame.K_d] and charRect.right < 1030:
+            charRect = charRect.move(15, 0)
+
+
+
+    enemyRect = enemyRect.move(-3, 0)
+    if charRect.colliderect(enemyRect) and playerHealth > 1 and not eHasHit:
+        playerHealth -= 1
+        eHasHit = True
+        print("player health:", playerHealth)
+    elif charRect.colliderect(enemyRect) and playerHealth == 1:
+        print("player dead")
+
+
     pygame.display.update()
+
+    # Slow the game to update 60 times per second
+    clock.tick(60)
 
 pygame.quit()
 
