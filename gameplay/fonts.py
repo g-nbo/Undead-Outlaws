096ecccefc62ff310b9ab0a60eceb095b63fb1e1
RH262 <rharr262@students.kennesaw.edu>
2024-11-12 20:44:30 -0500

    Update fonts.py

--- gameplay/fonts.py
+++ gameplay/fonts.py
@@ -5,9 +5,6 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.6)
 
 pygame.init()
 
-gameFont = pygame.font.SysFont('segoeprint', 64)
-game_overFont = pygame.font.SysFont(None, 200)
-
 textfont = pygame.font.SysFont(None, 60)
 instructText = textfont.render("A and D to move", 1, (255, 255, 255))
 instructText2 = textfont.render("left and right", 1, (255, 255, 255))
